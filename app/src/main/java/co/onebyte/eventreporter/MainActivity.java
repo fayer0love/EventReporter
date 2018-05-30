a905c2dc5592f9b92e2c3f5e41a8856dc8ae408b
Shiyang Li <sli99@hawk.iit.edu>
2018-05-29 23:04:45 -0500

    second commit

--- app/src/main/java/co/onebyte/eventreporter/MainActivity.java
+++ app/src/main/java/co/onebyte/eventreporter/MainActivity.java
@@ -2,6 +2,7 @@ package co.onebyte.eventreporter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("Life cycle test", "We are at onCreate()");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("Life cycle test", "We are at onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("Life cycle test", "We are at onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("Life cycle test", "We are at onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("Life cycle test", "We are at onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("Life cycle test", "We are at onDestroy()");
+    }
+
 }
