66e0f5de47fd05b61b25133770f125b06b16c72a
Shiyang Li <sli99@hawk.iit.edu>
2018-05-29 23:52:31 -0500

    add 2 second activity

--- app/src/main/java/co/onebyte/eventreporter/MainActivity.java
+++ app/src/main/java/co/onebyte/eventreporter/MainActivity.java
@@ -1,16 +1,31 @@
 package co.onebyte.eventreporter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("Life cycle test", "We are at onCreate()");
+        TextView view = findViewById(R.id.text);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                MainActivity.this.startActivity(intent);
+                //MainActivity.this.finish();
+
+            }
+        });
+
     }
 
     @Override
