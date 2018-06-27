2fbe889c994d93dac12840a3b4e4f8273ecc322a
Shiyang Li <sli99@hawk.iit.edu>
2018-06-26 23:16:24 -0500

    adding navigation bar

--- app/src/androidTest/java/co/onebyte/eventreporter/ExpressoTestSample.java
+++ app/src/androidTest/java/co/onebyte/eventreporter/ExpressoTestSample.java
@@ -1,4 +1,4 @@
-package com.laioffer.eventreporter;
+package co.onebyte.eventreporter;
 
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.filters.LargeTest;
@@ -11,15 +11,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import co.onebyte.eventreporter.LoginActivity;
-import co.onebyte.eventreporter.R;
-
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static android.support.test.rule.ActivityTestRule;
 
 
 @RunWith(AndroidJUnit4.class)
